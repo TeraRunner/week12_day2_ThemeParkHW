1d1e13e70fe23b1b48d3e83de3b616a99fe7049e
Ruben Franco <rubenfrancosanchez@hotmail.es>
2019-04-02 21:01:38 +0100

    Stall done

---
+++ src/main/java/Stalls/IcecreamStall.java
@@ -0,0 +1,9 @@
+package Stalls;
+
+public class IcecreamStall extends Stall{
+
+    public IcecreamStall(String name, String ownerName, int parkingSpot) {
+        super(name, ownerName, parkingSpot);
+    }
+
+}
---
+++ src/main/java/Stalls/TobaccoStall.java
@@ -0,0 +1,9 @@
+package Stalls;
+
+public class TobaccoStall extends Stall {
+
+    public TobaccoStall(String name, String ownerName, int parkingSlot) {
+        super(name, ownerName, parkingSlot);
+    }
+
+}
